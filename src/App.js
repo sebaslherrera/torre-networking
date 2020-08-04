e3e3f7a6d2fc754dc8b6a8d97c3b0fba6cf97b47
Sebastian L <lopez.sebastian@correounivalle.edu.co>
2020-08-04 18:15:41 -0500

    Working on rendering the skills

--- src/App.js
+++ src/App.js
@@ -5,14 +5,30 @@ import {
   Redirect,
   Switch,
 } from "react-router-dom";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
 import "./App.css";
 
 import Mentors from "./mentor/pages/Mentors";
 
+const useStyles = makeStyles((theme) => ({
+  offset: theme.mixins.toolbar,
+}));
+
 function App() {
+  const classes = useStyles();
   return (
     <Router>
-      {/* <MainNavigation /> */}
+      <AppBar style={{ backgroundColor: "#27292D" }}>
+        <Toolbar variant="dense">
+          <Typography variant="h6" color="inherit">
+            Torre Networking
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <div className={classes.offset} />
       <main>
         <Switch>
           <Route path="/" exact>
--- src/mentor/pages/Mentors.js
+++ src/mentor/pages/Mentors.js
@@ -2,6 +2,10 @@ import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import Paper from "@material-ui/core/Paper";
 import Axios from "axios";
 
 const useStyles = makeStyles((theme) => ({
@@ -14,21 +18,40 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const Mentors = () => {
-  const [username, setUsername] = useState(null);
   const classes = useStyles();
+  const [username, setUsername] = useState(null);
+  const [skills, setSkills] = useState(null);
+  const [mentors, setMentors] = useState(null);
 
   const handleUsername = (e) => {
     if (!e.target.value.trim()) return;
     setUsername(e.target.value);
+    setMentors(null);
   };
 
-  const getMentors = async (e) => {
+  const getSkills = async (e) => {
     e.preventDefault();
     if (username) {
       try {
         const response = await Axios.get(
           `http://localhost:5000/api/${username}`
         );
+        setSkills(response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  };
+
+  const getMentors = async (e) => {
+    e.preventDefault();
+    const skill = e.currentTarget.value;
+    if (skill) {
+      try {
+        const response = await Axios.get(
+          `http://localhost:5000/api/mentor/${skill}`
+        );
+        setMentors(response.data);
         console.log(response.data);
       } catch (err) {
         console.log(err);
@@ -37,29 +60,69 @@ const Mentors = () => {
   };
 
   return (
-    <form
-      className={classes.root}
-      noValidate
-      autoComplete="off"
-      onSubmit={getMentors}
-    >
-      <TextField
-        id="standard-basic"
-        label="Username of Torre"
-        name="username"
-        onChange={handleUsername}
-        autoFocus
-      />
-      <Button
-        variant="contained"
-        color="primary"
-        name="username"
-        type="submit"
-        // onClick={getMentors}
+    <React.Fragment>
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        padding={1}
       >
-        Search
-      </Button>
-    </form>
+        <Paper m="auto" bgcolor="lightblue">
+          <form
+            className={classes.root}
+            noValidate
+            autoComplete="off"
+            onSubmit={getSkills}
+          >
+            <TextField
+              id="standard-basic"
+              label="Username of Torre"
+              name="username"
+              onChange={handleUsername}
+              autoFocus
+            />
+            <Button
+              variant="contained"
+              color="primary"
+              name="username"
+              type="submit"
+            >
+              Search
+            </Button>
+          </form>
+        </Paper>
+      </Box>
+      <Container fixed>
+        <Grid container spacing={3}>
+          {skills &&
+            skills.map((skill) => {
+              return (
+                <Grid item xs={3} sm={3} key={skill.id}>
+                  <Button
+                    size="medium"
+                    color="primary"
+                    variant="outlined"
+                    onClick={getMentors}
+                    value={skill.name}
+                  >
+                    {skill.name}
+                  </Button>
+                </Grid>
+              );
+            })}
+        </Grid>
+      </Container>
+      <div>
+        {mentors &&
+          mentors.map((mentor) => {
+            return (
+              <li key={mentor.subjectId} id={mentor.subjectId}>
+                {mentor.username}
+              </li>
+            );
+          })}
+      </div>
+    </React.Fragment>
   );
 };
 
