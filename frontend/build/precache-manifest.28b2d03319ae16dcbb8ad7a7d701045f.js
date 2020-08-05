0be58c269fd54d32b37b739aa39ff159f57e0a13
Sebastian L <lopez.sebastian@correounivalle.edu.co>
2020-08-05 11:57:59 -0500

    Added deploy of frontend through docker

---
+++ frontend/build/precache-manifest.28b2d03319ae16dcbb8ad7a7d701045f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bb7f1303e33ed6f0694c1a622065a15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32cc44d0e5b55f7dda2b",
+    "url": "/static/css/main.2447cb68.chunk.css"
+  },
+  {
+    "revision": "7c336c753057ea6fe039",
+    "url": "/static/js/2.67f8b59c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.67f8b59c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32cc44d0e5b55f7dda2b",
+    "url": "/static/js/main.815afbee.chunk.js"
+  },
+  {
+    "revision": "7048a6c05a710704aef4",
+    "url": "/static/js/runtime-main.74da344a.js"
+  }
+]);
